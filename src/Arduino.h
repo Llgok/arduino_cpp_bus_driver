970cab4fcb84085d9232190ebdf602b12ac99e10
LILYGO_L <13073925292@163.com>
2025-11-20 17:48:11 +0800

    update PI

--- src/Arduino.h
+++ src/Arduino.h
@@ -2,7 +2,7 @@
  * @Description: Arduino
  * @Author: LILYGO_L
  * @Date: 2025-08-05 13:41:44
- * @LastEditTime: 2025-08-09 11:44:34
+ * @LastEditTime: 2025-11-20 15:33:57
  * @License: GPL 3.0
  */
 #pragma once
@@ -12,5 +12,7 @@
 #include "arduino_cpp_bus_driver_library.h"
 
 #include "Print.h"
+
+#define PI 3.1415926535897932384626433832795
 
 #define sq(x) ((x) * (x))
